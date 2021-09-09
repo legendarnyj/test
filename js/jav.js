3f49e0086fc16476118f614221cd4a89f28d1a7e
Alexey <xa4uk.s.nojom@gmail.ru>
2021-09-09 17:37:01 +0300

    java up

--- js/jav.js
+++ js/jav.js
@@ -2,5 +2,18 @@
 // alert("ATROPOOOOOOOOOOOOS");
 // alert(7+7*7); комент просто так?
 let jag;//обьявили переменную
-jag ="Jaggernaut"// дали значение к переменной
+jag ="Jaggernaut"// дали значение к переменной, можно и так:let jag ="etc";
 alert(jag);
+let numberOne = 77;
+let numberTwo = 23;
+alert(numberOne+numberTwo);
+// const NumberThree = 100;
+// const orange ="#FF7F00" пример константы для цветов
+// let color = orange;
+// alert(color);
+
+let admin;
+let name;
+name = "Jonny";
+admin = name;
+alert(admin);
