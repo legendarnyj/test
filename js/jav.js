00aa12a63d1b485e6a708622284cddeecec53b79
Alexey <Stells92@mail.ru>
2021-09-10 00:02:55 +0300

    java 10.09

--- js/jav.js
+++ js/jav.js
@@ -1,19 +1,48 @@
 // alert("JAGERNAAAAAAAAAAAAAAAAUTT");
 // alert("ATROPOOOOOOOOOOOOS");
 // alert(7+7*7); комент просто так?
-let jag;//обьявили переменную
-jag ="Jaggernaut"// дали значение к переменной, можно и так:let jag ="etc";
-alert(jag);
-let numberOne = 77;
-let numberTwo = 23;
-alert(numberOne+numberTwo);
+// let jag;//обьявили переменную
+// jag ="Jaggernaut"// дали значение к переменной, можно и так:let jag ="etc";
+// alert(jag);
+// let numberOne = 77;
+// let numberTwo = 23;
+// alert(numberOne+numberTwo);
 // const NumberThree = 100;
+
 // const orange ="#FF7F00" пример константы для цветов
 // let color = orange;
 // alert(color);
 
-let admin;
-let name;
-name = "Jonny";
-admin = name;
-alert(admin);
+// let admin;
+// let name;
+// name = "Jonny";
+// admin = name;
+// alert(admin);
+
+// let name = "Sirega"; возвращает имя с постоянным элементом
+// alert(`Black Buma ,${name}!`);
+
+// let bolshe = 200<199; true/false переменная
+// alert(bolshe);
+// let ravno = 4<5;
+// alert(ravno);
+
+// let uName = prompt('What your name');
+// alert(`Твой имя ${uName}!`); //промт дает вбить что то и потом возврщает вбитое значение с фразой
+// let boys = confirm('you love boys?');
+// alert(boys);// если ок - true
+
+// Преобразование типов урок повторить, ничего не понимаю про valut,typeof 
+// alert("8"-"2");
+
+// alert(2767/265);
+// alert(2767%265);
+// let x = 10;
+// alert(x**2);
+// alert( 2 + 1 );
+
+// let right = "5"; унарные бинарные правила + перед строкой плюсует значения, без + они перед данными они не складываются
+// let left = "25";
+// alert(+right+ +left);
+
+// Base operators, math
