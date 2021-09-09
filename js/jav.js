800f0b1ac806c2088e2eb37c0d3d2aba9d4f1ef1
Alexey <xa4uk.s.nojom@gmail.ru>
2021-09-09 17:04:38 +0300

    few add

---
+++ js/jav.js
@@ -0,0 +1,6 @@
+// alert("JAGERNAAAAAAAAAAAAAAAAUTT");
+// alert("ATROPOOOOOOOOOOOOS");
+// alert(7+7*7); комент просто так?
+let jag;//обьявили переменную
+jag ="Jaggernaut"// дали значение к переменной
+alert(jag);
